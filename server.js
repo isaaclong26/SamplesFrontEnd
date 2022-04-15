851d56691fb2b30848a0c86685956a195eb86c89
Isaac Long <isaaclong123@outlook.com>
2022-04-15 10:28:37 -0400

    server.js edit

--- server.js
+++ server.js
@@ -287,9 +287,7 @@ app.get("/allRequests", async (req, res)=>{
 
 
 })
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "Public", "build", "index.html"));
-});
+
 app.listen((process.env.PORT || 5000), () => {
   console.log(`App running on port ${PORT}!`);
 });
